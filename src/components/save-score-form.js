9d487afd157cf8b8323b3e798a7fb03dd9821a45
Luke Bennett <hello@lukebennett.com.au>
2020-02-01 23:33:44 +1100

    Style save score form

--- src/components/save-score-form.js
+++ src/components/save-score-form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { navigate } from 'gatsby';
+import { Link, navigate } from 'gatsby';
 
 import { useFirebase } from './firebase/firebase-context';
 
@@ -29,18 +29,34 @@ export default function SaveScoreForm({ score }) {
     <div className="w-full px-6 py-3">
       <div className="max-w-3xl">
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={username}
-            onChange={handleChange}
-            className="input"
-          />
-          <br />
-          <button type="submit" disabled={!username} className="mt-6 button">
-            Save
-          </button>
+          <h2 className="text-4xl font-bold text-center">Submit high score</h2>
+          <div className="flex">
+            <label htmlFor="username">
+              <span className="text-sm font-bold tracking-wider uppercase">
+                Username
+              </span>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={username}
+                onChange={handleChange}
+                className="input"
+              />
+            </label>
+            <button
+              type="submit"
+              disabled={!username}
+              className="mt-6 ml-2 button"
+            >
+              Save
+            </button>
+          </div>
+          <p className="text-center">
+            <Link to="/" className="mt-12 button">
+              Return home
+            </Link>
+          </p>
         </form>
       </div>
     </div>
