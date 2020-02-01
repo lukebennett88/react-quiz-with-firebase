a8a360fb3cb58fb026ea5e74d66f208aae0845a7
Luke Bennett <hello@lukebennett.com.au>
2020-02-01 23:42:06 +1100

    Display high scores

--- src/pages/high-scores.js
+++ src/pages/high-scores.js
@@ -1,20 +1,60 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
 import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { useFirebase } from '../components/firebase/firebase-context';
+import { Loader } from '../components/loader';
 
 export default function HighScores() {
+  const [scores, setScores] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const firebase = useFirebase();
+
+  React.useEffect(() => {
+    firebase.scores().once('value', snapshot => {
+      const data = snapshot.val();
+
+      const formatScores = firebaseScores => {
+        const formattedScores = [];
+        for (const key in firebaseScores) {
+          const val = firebaseScores[key];
+          val.key = key;
+          formattedScores.push(val);
+        }
+        return formattedScores
+          .sort((score2, score1) => score1.score - score2.score)
+          .slice(0, 10);
+      };
+
+      const sortedScores = formatScores(data);
+      setScores(sortedScores);
+      setLoading(false);
+    });
+  }, [firebase, scores]);
+
   return (
     <Layout>
       <SEO title="High Scores" />
-      <h1 className="text-4xl font-bold">High Scores</h1>
-      <Link to="/game" className="mt-6 button">
-        Start Game
-      </Link>
-      <Link to="/high-scores" className="mt-2 button">
-        High Scores
-      </Link>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1 className="text-4xl font-bold">High Scores</h1>
+          <ul>
+            {scores.map(score => (
+              <li key={score.key}>
+                {score.name}: {score.score}
+              </li>
+            ))}
+          </ul>
+          <Link to="/game" className="mt-6 button">
+            New Game
+          </Link>
+        </>
+      )}
     </Layout>
   );
 }
