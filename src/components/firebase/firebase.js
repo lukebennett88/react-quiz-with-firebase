71eaae7b8911b5e4733bd4ded08edd3d6ea8b421
Luke Bennett <hello@lukebennett.com.au>
2020-02-02 21:38:07 +1100

    Try to fix environment variable's not loading

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,13 +1,20 @@
 import app from 'firebase/app';
 import 'firebase/database';
 
+const apiKey = process.env.GATSBY_API_KEY;
+const authDomain = process.env.GATSBY_AUTH_DOMAIN;
+const databaseURL = process.env.GATSBY_DATABASE_URL;
+const projectId = process.env.GATSBY_PROJECT_ID;
+const storageBucket = process.env.GATSBY_STORAGE_BUCKET;
+const messagingSenderId = process.env.GATSBY_MESSAGING_SENDER_ID;
+
 const config = {
-  apiKey: process.env.GATSBY_API_KEY,
-  authDomain: process.env.GATSBY_AUTH_DOMAIN,
-  databaseURL: process.env.GATSBY_DATABASE_URL,
-  projectId: process.env.GATSBY_PROJECT_ID,
-  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
-  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
 };
 
 class Firebase {
