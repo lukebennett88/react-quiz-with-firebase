681a91ab2f59b52a565b7afddb2ea706b806e596
Luke Bennett <hello@lukebennett.com.au>
2020-02-02 21:55:11 +1100

    Revert

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,20 +1,13 @@
 import app from 'firebase/app';
 import 'firebase/database';
 
-const apiKey = process.env.GATSBY_API_KEY;
-const authDomain = process.env.GATSBY_AUTH_DOMAIN;
-const databaseURL = process.env.GATSBY_DATABASE_URL;
-const projectId = process.env.GATSBY_PROJECT_ID;
-const storageBucket = process.env.GATSBY_STORAGE_BUCKET;
-const messagingSenderId = process.env.GATSBY_MESSAGING_SENDER_ID;
-
 const config = {
-  apiKey,
-  authDomain,
-  databaseURL,
-  projectId,
-  storageBucket,
-  messagingSenderId,
+  apiKey: process.env.GATSBY_API_KEY,
+  authDomain: process.env.GATSBY_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_DATABASE_URL,
+  projectId: process.env.GATSBY_PROJECT_ID,
+  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
