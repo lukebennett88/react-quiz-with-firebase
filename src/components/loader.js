af59ec69689ce7095329d43c0cd9fb250d69049d
Luke Bennett <hello@lukebennett.com.au>
2020-02-09 14:16:32 +1100

    Use SVG loader instead of empty div

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Spinner from 'react-svg-spinner';
+import tw from 'tailwindcss/defaultTheme';
 
 export const Loader = () => {
-  return <div className="loader" />;
+  return <Spinner size="4rem" color={tw.colors.blue[600]} thickness={3} />;
 };
